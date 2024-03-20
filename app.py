e74a04f7e4345c82a308191a7f7f318e0e65f90c
divyachatrathi0406 <112478418+divyachatrathi0406@users.noreply.github.com>
2024-03-20 11:39:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import pickle
 import numpy as np
 import streamlit as st
 import pandas as pd
-import sklearn
 #============================================================
 # custome and helping functions
 #==========================helper funtions================
